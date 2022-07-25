def970326437f3edf3c921dd514d9831f4f852ab
최한빈 <binn@choehanbin-ui-MacBookPro.local>
2022-07-26 00:15:45 +0900

    Initial commit

---
+++ src/main/java/com/binn/oha/OHaApplication.java
@@ -0,0 +1,13 @@
+package com.binn.oha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OHaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OHaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/binn/oha/OHaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binn.oha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OHaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
